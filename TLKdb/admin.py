aad730a95734e52d4974e60a39207ba576f0592a
skug <mr.skug@gmail.com>
2014-11-06 10:17:53 +0200

    Initial Database structure

--- TLKdb/admin.py
+++ TLKdb/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from TLKdb.models import Person, Member, MemberType, Merit, MeritType, Committee, CommitteeType, Official, OfficialType, Board, BoardPosition
+
+
+admin.site.register(Person)
+admin.site.register(Member)
+admin.site.register(MemberType)
+admin.site.register(Merit)
+admin.site.register(MeritType)
+admin.site.register(Committee)
+admin.site.register(CommitteeType)
+admin.site.register(Official)
+admin.site.register(OfficialType)
+admin.site.register(Board)
+admin.site.register(BoardPosition)
--- TLKdb/models.py
+++ TLKdb/models.py
@@ -1,3 +1,115 @@
 from django.db import models
 
-# Create your models here.
+
+# Medlemstyper
+class MemberType(models.Model):
+    name = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.name
+
+
+# Styrelseposter
+class BoardPosition(models.Model):
+    name = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.name
+
+
+# Utskott
+class CommitteeType(models.Model):
+    name = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.name
+
+
+# Funktionärer
+class OfficialType(models.Model):
+    name = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.name
+
+
+# Förtjänsttecken (Guld, Silver, SGRS, Årets Werkeit, Storwerket, Hedersmedlem)
+class MeritType(models.Model):
+    name = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.name
+
+# Personer
+class Person(models.Model):
+    firstname = models.CharField(max_length=45)
+    middlenames = models.CharField(max_length=100, blank=True)
+    lastname = models.CharField(max_length=45)
+    dob = models.DateField(null=True, blank=True)
+    dod = models.DateField(null=True, blank=True)
+    birthplace = models.CharField(max_length=120, blank=True)
+    email = models.CharField(max_length=120)
+    address = models.CharField(max_length=100, blank=True)
+    city = models.CharField(max_length=180, blank=True)
+    zip = models.CharField(max_length=20, blank=True)
+    country = models.CharField(max_length=45, blank=True)
+    joined = models.DateField(null=True, blank=True)
+    graduated = models.DateField(null=True, blank=True)
+    company = models.CharField(max_length=45, blank=True)
+    company_email = models.CharField(max_length=120, blank=True)
+    company_phone = models.CharField(max_length=45, blank=True)
+    notes = models.TextField(max_length=1000, blank=True)
+
+#    member = models.ManyToManyField(MemberType)
+#    board = models.ManyToManyField(BoardPosition)
+#    committee = models.ManyToManyField(CommitteeType)
+#    official = models.ManyToManyField(OfficialType)
+#    merit = models.ManyToManyField(MeritType)
+
+    def __str__(self):
+        return '%s %s' % (self.firstname, self.lastname)
+
+
+class Member(models.Model):
+    year = models.DateField()
+    type = models.ForeignKey(MemberType, related_name='members')
+    person = models.ForeignKey(Person, related_name='members')
+
+    def __str__(self):
+        return '%s %s %s' % (self.year, self.type, self.person)
+
+
+class Board(models.Model):
+    year = models.DateField()
+    type = models.ForeignKey(BoardPosition, related_name='boards')
+    person = models.ForeignKey(Person, related_name='boards')
+
+    def __str__(self):
+        return '%s %s %s' % (self.year, self.type, self.person)
+
+
+class Official(models.Model):
+    year = models.DateField()
+    type = models.ForeignKey(OfficialType, related_name='officials')
+    person = models.ForeignKey(Person, related_name='officials')
+
+    def __str__(self):
+        return '%s %s %s' % (self.year, self.type, self.person)
+
+
+class Merit(models.Model):
+    year = models.DateField()
+    type = models.ForeignKey(MeritType, related_name='merits')
+    person = models.ForeignKey(Person, related_name='merits')
+
+    def __str__(self):
+        return '%s %s %s' % (self.year, self.type, self.person)
+
+
+class Committee(models.Model):
+    year = models.DateField()
+    type = models.ForeignKey(CommitteeType, related_name='committees')
+    person = models.ForeignKey(Person, related_name='committees')
+
+    def __str__(self):
+        return '%s %s %s' % (self.year, self.type, self.person)
