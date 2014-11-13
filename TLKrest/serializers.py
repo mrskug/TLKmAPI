08383c094ec81110bb3e70a106bf4c68540e7854
skug <mr.skug@gmail.com>
2014-11-13 19:36:15 +0200

    Serializers for more models, API taking shape :)

--- TLKrest/serializers.py
+++ TLKrest/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from TLKdb.models import Person
+from TLKdb.models import *
 
 
 # Serializers define the API representation.
@@ -12,3 +12,56 @@ class PersonSerializer(serializers.HyperlinkedModelSerializer):
                   'address', 'city', 'zip', 'country',
                   'joined', 'graduated', 'company',
                   'company_email', 'company_phone', 'notes')
+
+
+class MemberSerializer(serializers.ModelSerializer):
+    type = serializers.SlugRelatedField(read_only=True, slug_field='name')
+    class Meta:
+        model = Member
+        fields = ('year', 'type')
+
+
+class BoardSerializer(serializers.ModelSerializer):
+    type = serializers.SlugRelatedField(read_only=True, slug_field='name')
+    class Meta:
+        model = Board
+        fields = ('year', 'type')
+
+
+class OfficialSerializer(serializers.ModelSerializer):
+    type = serializers.SlugRelatedField(read_only=True, slug_field='name')
+    class Meta:
+        model = Official
+        fields = ('year', 'type')
+
+
+class MeritSerializer(serializers.ModelSerializer):
+    type = serializers.SlugRelatedField(read_only=True, slug_field='name')
+    class Meta:
+        model = Merit
+        fields = ('year', 'type')
+
+
+class CommitteeSerializer(serializers.ModelSerializer):
+    type = serializers.SlugRelatedField(read_only=True, slug_field='name')
+    class Meta:
+        model = Committee
+        fields = ('year', 'type')
+
+
+class EverythingSerializer(serializers.ModelSerializer):
+    members = MemberSerializer(many=True)
+    boards = BoardSerializer(many=True)
+    officials = OfficialSerializer(many=True)
+    merits = MeritSerializer(many=True)
+    committees = CommitteeSerializer(many=True)
+
+    class Meta:
+        model = Person
+        fields = ('firstname', 'middlenames', 'lastname',
+                  'dob', 'dod', 'birthplace', 'email',
+                  'address', 'city', 'zip', 'country',
+                  'joined', 'graduated', 'company',
+                  'company_email', 'company_phone',
+                  'notes', 'members', 'boards',
+                  'officials', 'merits', 'committees')
--- TLKrest/views.py
+++ TLKrest/views.py
@@ -1,9 +1,9 @@
 from rest_framework import viewsets
-from TLKrest.serializers import PersonSerializer
-from TLKdb.models import Person
+from TLKrest.serializers import EverythingSerializer
+from TLKdb.models import *
 
 
 # ViewSets define the view behavior.
 class PersonViewSet(viewsets.ModelViewSet):
     queryset = Person.objects.all()
-    serializer_class = PersonSerializer
+    serializer_class = EverythingSerializer
