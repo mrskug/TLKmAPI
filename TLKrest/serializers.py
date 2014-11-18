677ae3c61c59f7c254918a84cf2d1ff9562b7b2c
skug <mr.skug@gmail.com>
2014-11-18 19:00:53 +0200

    Api documentation example, create() & update() skeleton functions.

--- TLKrest/serializers.py
+++ TLKrest/serializers.py
@@ -4,15 +4,6 @@ from TLKdb.models import *
 
 
 # Serializers define the API representation.
-class PersonSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Person
-        fields = ('firstname', 'middlenames', 'lastname',
-                  'dob', 'dod', 'birthplace', 'email',
-                  'address', 'city', 'zip', 'country',
-                  'joined', 'graduated', 'company',
-                  'company_email', 'company_phone', 'notes')
-
 
 class MemberSerializer(serializers.ModelSerializer):
     type = serializers.SlugRelatedField(read_only=True, slug_field='name')
@@ -49,7 +40,7 @@ class CommitteeSerializer(serializers.ModelSerializer):
         fields = ('year', 'type')
 
 
-class EverythingSerializer(serializers.ModelSerializer):
+class PersonSerializer(serializers.ModelSerializer):
     members = MemberSerializer(many=True)
     boards = BoardSerializer(many=True)
     officials = OfficialSerializer(many=True)
@@ -65,3 +56,21 @@ class EverythingSerializer(serializers.ModelSerializer):
                   'company_email', 'company_phone',
                   'notes', 'members', 'boards',
                   'officials', 'merits', 'committees')
+
+        # Untested !!
+        def create(self, validated_data):
+            members_data = validated_data.pop['members']
+            boards_data = validated_data.pop['boards']
+            officials_data = validated_data.pop['officials']
+            merits_data = validated_data.pop['merits']
+            committees_data = validated_data.pop['committees']
+            person = Person.objects.create(**validated_data)
+            Member.objects.create(person=person, **members_data)
+            Board.objects.create(person=person, **boards_data)
+            Official.objects.create(person=person, **officials_data)
+            Merit.objects.create(person=person, **merits_data)
+            Committee.objects.create(person=person, **committees_data)
+            return person
+
+        def update(self, instance, validated_data):
+            pass
--- TLKrest/views.py
+++ TLKrest/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
-from TLKrest.serializers import EverythingSerializer
+from TLKrest.serializers import PersonSerializer
 from TLKdb.models import *
 
 
 # ViewSets define the view behavior.
 class PersonViewSet(viewsets.ModelViewSet):
+    """
+    Derp Derp Derpity Derp [Herp][ref]
+
+    [ref]: http://derp.derp.fi/derp
+    """
     queryset = Person.objects.all()
-    serializer_class = EverythingSerializer
+    serializer_class = PersonSerializer
