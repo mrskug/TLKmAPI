eca1a07ca7da251b3ae14e43d98eb00bae65de0c
Christoffer Holmberg <mr.skug@gmail.com>
2015-02-04 12:37:52 +0200

    disabled  b0rked OAuth2 provider -_-

--- TLKmAPI/settings.py
+++ TLKmAPI/settings.py
@@ -61,9 +61,9 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        #'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-        'oauth2_provider.ext.rest_framework.OAuth2Authentication',
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        #oauth2_provider.ext.rest_framework.OAuth2Authentication',
+        #'rest_framework.permissions.IsAuthenticatedOrReadOnly'
     ]
 }
 
