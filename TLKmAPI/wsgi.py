04e41ca7ebbc07155c7a51d3c3a5b3fa73707b92
blah <blah>
2015-03-30 17:37:42 +0300

    modified for deploymet with apache mod_wsgi

--- TLKmAPI/wsgi.py
+++ TLKmAPI/wsgi.py
@@ -7,8 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TLKmAPI.settings")
+import os, sys
+sys.path.append('/srv/TLKmAPI/')
+os.environ["DJANGO_SETTINGS_MODULE"] = "TLKmAPI.settings"
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
