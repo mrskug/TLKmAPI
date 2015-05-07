ae0846c9afa818b9af1369fbf881d46c5979000f
skug <mr.skug@gmail.com>
2015-05-07 13:12:17 +0300

    mysql support example

--- TLKmAPI/settings.py
+++ TLKmAPI/settings.py
@@ -94,7 +94,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    },
+    #'mysql': {
+    #    'ENGINE': 'django.db.backends.mysql',
+    #    'OPTIONS': {
+    #        'read_default_file': './my.cnf',
+    #    },
+    #}
 }
 
 # Internationalization
